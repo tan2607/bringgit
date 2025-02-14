b41c0c39942b60de5d99591ca33c4e27c42ffcc6
Duy Nguyen <duy@keyreply.com>
2025-02-14 11:04:45 +0700

    Add error handler

--- server/api/tasks/schedule.get.ts
+++ server/api/tasks/schedule.get.ts
@@ -1,23 +1,31 @@
 import { jobQueue } from "~/server/database/schema";
 
 export default defineEventHandler(async (event) => {
-	const db = useDrizzle();
-	const jobQueues = await db.query.jobQueue.findMany({
-		limit: 10,
-		where: and(eq(jobQueue.status, "pending")),
-		orderBy: [desc(jobQueue.createdAt)],
-	});
-
-	await $fetch("/api/queue/consumer", {
-		method: "POST",
-		body: {
-			messages: jobQueues,
-		},
-	});
-
-	console.log("[Schedule] Job queues scheduled");
-
-	return {
-		result: "success",
-	};
+	try {
+		const db = useDrizzle();
+		const jobQueues = await db.query.jobQueue.findMany({
+			limit: 10,
+			where: and(eq(jobQueue.status, "pending")),
+			orderBy: [desc(jobQueue.createdAt)],
+		});
+	
+		await $fetch("/api/queue/consumer", {
+			method: "POST",
+			body: {
+				messages: jobQueues,
+			},
+		});
+	
+		console.log("[Schedule] Job queues scheduled");
+	
+		return {
+			result: "success",
+		};
+	} catch (error) {
+		console.error("[Schedule] Error scheduling job queues", error);
+		return {
+			result: "error",
+			error: error,
+		};
+	}
 });
