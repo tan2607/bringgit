a6a4d0d9e1e0481cb3a52b4c826d34240283fe53
Duy Nguyen <duy@keyreply.com>
2025-02-17 11:49:17 +0700

    Implement function for menu action of single job

---
+++ server/api/jobs/[id].delete.ts
@@ -0,0 +1,21 @@
+import { jobQueue, jobs } from "~/server/database/schema";
+
+export default defineEventHandler(async (event) => {
+	try {
+		const jobId = getRouterParam(event, "id");
+		const db = useDrizzle();
+		// Delete the job from the database and the job_queue related to it
+		const targetJob = await db.query.jobs.findFirst({
+			where: eq(jobs.id, jobId as string),
+		});
+		if (!targetJob) {
+			return { success: false, message: "Job not found" };
+		}
+		await db.delete(jobQueue).where(eq(jobQueue.jobId, jobId as string));
+		await db.delete(jobs).where(eq(jobs.id, jobId as string));
+		return { success: true, message: "Job deleted successfully" };
+	} catch (error) {
+		console.error(error);
+		return { success: false, message: "Error deleting job" };
+	}
+});
