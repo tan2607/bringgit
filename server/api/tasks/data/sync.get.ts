5cca7b5a9b8ff9aa65f3e6662dd8b386007e8dab
Duy Nguyen <duy@keyreply.com>
2025-06-19 00:03:23 +0700

    Recude loop times to 5

--- server/api/tasks/data/sync.get.ts
+++ server/api/tasks/data/sync.get.ts
@@ -2,64 +2,78 @@
 import { vapiCallData } from "~~/server/database/schema";
 import { formatCalls, syncCalls } from "~~/server/utils/fetchCall";
 import { getLastSyncedAt, setLastSyncedAt } from "~~/server/utils/syncState";
+import { desc } from "drizzle-orm";
 
 export default defineEventHandler(async (event) => {
   const db = useDrizzle();
-  const timeWindowMinutes = 30;
-
   let startDate = await getLastSyncedAt(db);
-  const now = new Date();
 
-  let insertedCount = 0;
-  let loop = 0;
+  try {
+    const timeWindowMinutes = 30;
 
-  while (true) {
-    const endDate = new Date(startDate);
-    endDate.setMinutes(endDate.getMinutes() + timeWindowMinutes);
+    const now = new Date();
 
-    if (endDate > now) break;
+    let loop = 0;
+    let insertedCount = 0;
 
-    const calls = await syncCalls(startDate.toISOString(), endDate.toISOString());
-    const formattedCalls = formatCalls(calls);
+    while (true) {
+      const endDate = new Date(startDate);
+      endDate.setMinutes(endDate.getMinutes() + timeWindowMinutes);
 
-    if (formattedCalls.length === 0) {
-      if (endDate.getHours() >= 19 || endDate.getHours() < 8) {
-        startDate = new Date(endDate.getTime() + 60 * 60 * 1000); // +1 hour
-      } else {
-        startDate = new Date(endDate.getTime() + 5 * 1000); // +5 seconds
+      if (endDate > now) break;
+
+      const calls = await syncCalls(
+        startDate.toISOString(),
+        endDate.toISOString()
+      );
+      const formattedCalls = formatCalls(calls);
+
+      if (formattedCalls.length === 0) {
+        if (endDate.getHours() >= 19 || endDate.getHours() < 8) {
+          startDate = new Date(endDate.getTime() + 60 * 60 * 1000); // +1 hour
+        } else {
+          startDate = new Date(endDate.getTime() + 5 * 1000); // +5 seconds
+        }
+        continue;
       }
-      continue;
-    }
 
-    const lastCall = formattedCalls[formattedCalls.length - 1];
-    const latest = new Date(lastCall.createdAt);
-    startDate = new Date(latest.getTime() + 10 * 1000);
+      const lastCall = formattedCalls[formattedCalls.length - 1];
+      const latest = new Date(lastCall.createdAt);
+      startDate = new Date(latest.getTime() + 10 * 1000);
 
-    try {
       await insertData(db, formattedCalls);
       insertedCount += formattedCalls.length;
-    } catch (err) {
-      console.error("Insert failed:", err);
-      // get last inserted call
-      const lastCallArr = await db.select().from(vapiCallData).orderBy(desc(vapiCallData.createdAt)).limit(1);
-      const lastCall = lastCallArr?.[0];
-      if (lastCall?.createdAt) {
-        const latest = new Date(lastCall.createdAt);
-        startDate = new Date(latest.getTime() + 10 * 1000);
-      }
-      break;
+
+      if (++loop > 5) break;
     }
 
-    if (++loop > 10) break;
+    await setLastSyncedAt(db, startDate);
+
+    return {
+      status: "success",
+      inserted: insertedCount,
+      nextStart: startDate.toISOString(),
+    };
+  } catch (error) {
+    console.error("Sync failed:", error);
+    const lastCallArr = await db
+      .select()
+      .from(vapiCallData)
+      .orderBy(desc(vapiCallData.createdAt))
+      .limit(1);
+    
+    const lastCall = lastCallArr?.[0];
+    if (lastCall?.createdAt) {
+      const latest = new Date(lastCall.createdAt);
+      startDate = new Date(latest.getTime() + 10 * 1000);
+    }
+    await setLastSyncedAt(db, startDate);
+    return {
+      status: "error",
+      error: error.message,
+      nextStart: startDate.toISOString(),
+    };
   }
-
-  await setLastSyncedAt(db, startDate);
-
-  return {
-    status: "success",
-    inserted: insertedCount,
-    nextStart: startDate.toISOString(),
-  };
 });
 
 async function insertData(db: ReturnType<typeof useDrizzle>, rows: any[]) {
