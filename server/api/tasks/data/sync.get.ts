c0b4c1e212aa1b57e8258415ad9c08cc9490372f
Duy Nguyen <duy@keyreply.com>
2025-06-18 23:13:25 +0700

    Fix insert large data error

--- server/api/tasks/data/sync.get.ts
+++ server/api/tasks/data/sync.get.ts
@@ -5,10 +5,10 @@ import { getLastSyncedAt, setLastSyncedAt } from "~~/server/utils/syncState";
 
 export default defineEventHandler(async (event) => {
   const db = useDrizzle();
-  const timeWindowMinutes = 30; // Adjust based on average volume
+  const timeWindowMinutes = 30;
 
   let startDate = await getLastSyncedAt(db);
-  const now = new Date(); // current time to compare against
+  const now = new Date();
 
   let insertedCount = 0;
   let loop = 0;
@@ -17,35 +17,40 @@ export default defineEventHandler(async (event) => {
     const endDate = new Date(startDate);
     endDate.setMinutes(endDate.getMinutes() + timeWindowMinutes);
 
-    // Break if the endDate goes beyond current time
-    if (endDate > now) {
-      break;
-    }
+    if (endDate > now) break;
 
-    const calls = await syncCalls(
-      startDate.toISOString(),
-      endDate.toISOString()
-    );
+    const calls = await syncCalls(startDate.toISOString(), endDate.toISOString());
     const formattedCalls = formatCalls(calls);
 
     if (formattedCalls.length === 0) {
       if (endDate.getHours() >= 19 || endDate.getHours() < 8) {
-        // plus 1 hour
-        startDate = new Date(endDate.getTime() + 60 * 60 * 1000);
+        startDate = new Date(endDate.getTime() + 60 * 60 * 1000); // +1 hour
       } else {
-        startDate = new Date(endDate.getTime() + 5 * 1000);
+        startDate = new Date(endDate.getTime() + 5 * 1000); // +5 seconds
       }
       continue;
     }
-
-    await insertData(db, formattedCalls);
-    insertedCount += formattedCalls.length;
 
     const lastCall = formattedCalls[formattedCalls.length - 1];
     const latest = new Date(lastCall.createdAt);
     startDate = new Date(latest.getTime() + 10 * 1000);
 
-    if (++loop > 10) break; // infinite loop protection
+    try {
+      await insertData(db, formattedCalls);
+      insertedCount += formattedCalls.length;
+    } catch (err) {
+      console.error("Insert failed:", err);
+      // get last inserted call
+      const lastCallArr = await db.select().from(vapiCallData).orderBy(desc(vapiCallData.createdAt)).limit(1);
+      const lastCall = lastCallArr?.[0];
+      if (lastCall?.createdAt) {
+        const latest = new Date(lastCall.createdAt);
+        startDate = new Date(latest.getTime() + 10 * 1000);
+      }
+      break;
+    }
+
+    if (++loop > 10) break;
   }
 
   await setLastSyncedAt(db, startDate);
